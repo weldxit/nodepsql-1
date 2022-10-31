58a651a5e25bedf236d322daef9a15b7c11f087a
subha <subhransubidyasagar7@gmail.com>
2022-10-31 16:38:59 +0530

    cors error resolved

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const cors=require('cors')
 
 const app=express();
 
-const cors= require('cors')
 app.use(cors())
-
-app.use(cors)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true,}))
