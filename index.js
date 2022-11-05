70773f4ef40916675d5489afc3d9694b73294ba5
Saurav-Mohanty <115469889+Saurav-Mohanty@users.noreply.github.com>
2022-11-05 12:33:09 +0530

    Api Update

--- index.js
+++ index.js
@@ -1,88 +1,59 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const db= require('./queries')
-const cors=require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./queries");
+const cors = require("cors");
 
-const app=express();
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true,}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000,()=>{console.log("server running on port 3000")})
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
 
 // get all category
-app.get('/category', db.getCategory)
+app.get("/category", db.getCategory);
 
 //get all subcategory by category id
-app.get('/subcategory/:categoryId', db.getSubCategory)
+app.get("/subcategory/:categoryId", db.getSubCategory);
 
 //get a user by user id
-app.get('/users/:userId',db.getUsersById)
+app.get("/users/:userId", db.getUsersById);
 
 //get all products by sub category
-app.get('/products/:subcategoryId',db.getProducts)
+app.get("/products/:subcategoryId", db.getProducts);
 
 //create a user account
-app.post('/users/sign_up', db.createUser)
+app.post("/users/sign_up", db.createUser);
 
 //login a user
-app.post('/users/login', db.loginUser)
+app.post("/users/login", db.loginUser);
 
 //create a seller user account by update upser
-app.put('/users/:id', db.updateUser)
+app.put("/users/:id", db.updateUser);
 
 //get recent products and category
-app.get('/recent', db.getRecentProducts)
+app.get("/recent", db.getRecentProducts);
 
 //add a product to product table
-app.post('products/catagory/:table_name', db.createProduct)
+app.post("products/catagory/:table_name", db.createProduct);
 
 //add a product to favorite table
-app.post('/add_favorite', db.addProductTOFavorite)
+app.post("/add_favorite", db.addProductTOFavorite);
 
 //get all favorite products bu user id
-app.get('/favorite/:userId', db.getFavoriteProducts)
+app.get("/favorite/:userId", db.getFavoriteProducts);
 
 //remove a product from favorite table
 
-
-
-
 // add a product to cart table
-app.post('/add_cart', db.addProductToCart)
+app.post("/add_cart", db.addProductToCart);
 
 //get all cart's products by user id
-app.get('/cart/:userId', db.getProductsFromCart)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/cart/:userId", db.getProductsFromCart);
 
 // app.get('/users/:id',db.getUserbyId)
 // app.post('/users',db.Createuser)
--- queries.js
+++ queries.js
@@ -1,245 +1,309 @@
-const pool= require('./pool')
+const pool = require("./pool");
 //get all users from database
 
-const getUsersById =(req,res)=>{
-    const userId = req.params.userId
-    pool.query('SELECT * FROM users WHERE id = $1', [userId],(error,result)=> {
-        if (error){
-            throw error;
-        }
+const getUsersById = (req, res) => {
+  const userId = req.params.userId;
+  pool.query("SELECT * FROM users WHERE id = $1", [userId], (error, result) => {
+    if (error) {
+      throw error;
+    }
 
-        res.status(200).send(result.rows)
-    })
-}
-
+    res.status(200).send(result.rows);
+  });
+};
 
 // login user from database
 
-const loginUser = (req,res)=> {
-    const body = req.body
-    pool.query('SELECT * FROM users WHERE email=$1 AND password=$2',[body.email, body.password],(error,result)=>{
-        if (error){
-            throw error;
-        }
-        if(result.rowCount > 0) {
-            res.status(200).send('User Available')
-        } else {
-            res.status(404).send('User Not Found')
-        }
-    })
-}
-
+const loginUser = (req, res) => {
+  const body = req.body;
+  pool.query(
+    "SELECT * FROM users WHERE email=$1 AND password=$2",
+    [body.email, body.password],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      if (result.rowCount > 0) {
+        res.status(200).send("User Available");
+      } else {
+        res.status(404).send("User Not Found");
+      }
+    }
+  );
+};
 
 //create a buyer user account
 
-const createUser=(req,res) =>{
-    const body = req.body
-    console.log(body);
-        pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [body.email, body.password], (error, result) => {
-        if (error){
-            throw error;
-        }
+const createUser = (req, res) => {
+  const body = req.body;
+  console.log(body);
+  pool.query(
+    "INSERT INTO users (email, password, phone, first_name, last_name) VALUES ($1, $2, $3, $4, $5)",
+    [body.email, body.password, body.phone, body.first_name, body.last_name],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).send('added successfully')
-    })
-}
+      res.status(200).send("added successfully");
+    }
+  );
+};
+
+
 
 //create a seller user account
 
-const updateUser=(req,res) =>{
-    const body = req.body
-    const id = req.params.id
-    console.log(body);
-    const userId = body.first_name.substring(0, 4).toUpperCase() + body.phone
-    pool.query('UPDATE users SET first_name = $1, last_name = $2, village = $3, city = $4, block = $5, district = $6, farmer_id = $7, aadhaar_id = $8, pincode = $9, phone = $10, user_id = $11, payment_id = $12 where id = $13',
-    [body.first_name, body.last_name, body.village, body.city, body.block, body.district, body.farmer_id, body.aadhaar_id, body.pincode, body.phone, userId, body.payment_id, id],
+const updateUser = (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+  console.log(body);
+  const userId = body.first_name.substring(0, 4).toUpperCase() + body.phone;
+  pool.query(
+    "UPDATE users SET first_name = $1, last_name = $2, village = $3, city = $4, block = $5, district = $6, farmer_id = $7, aadhaar_id = $8, pincode = $9, phone = $10, user_id = $11, payment_id = $12 where id = $13",
+    [
+      body.first_name,
+      body.last_name,
+      body.village,
+      body.city,
+      body.block,
+      body.district,
+      body.farmer_id,
+      body.aadhaar_id,
+      body.pincode,
+      body.phone,
+      userId,
+      body.payment_id,
+      id,
+    ],
     (error, result) => {
-        if (error){
-            throw error;
-        }
-        res.status(200).send('updated successfully')
-    })
-}
-
+      if (error) {
+        throw error;
+      }
+      res.status(200).send("updated successfully");
+    }
+  );
+};
 
 // get all category items
 
-const getCategory =(req,res)=> {
-    pool.query('SELECT * FROM category',(error,result)=>{
-        if (error){
-            throw error;
-        }
+const getCategory = (req, res) => {
+  pool.query("SELECT * FROM category", (error, result) => {
+    if (error) {
+      throw error;
+    }
 
-        res.status(200).send(result.rows)
-    })
-}
+    res.status(200).send(result.rows);
+  });
+};
 
 // get all subcategory items based on category id
 
-const getSubCategory =(req,res)=> {
-    const categoryId = req.params.categoryId;
-    pool.query('SELECT * FROM subcategory WHERE category_id = $1', [categoryId], (error,result)=>{
-        if (error){
-            throw error;
-        }
+const getSubCategory = (req, res) => {
+  const categoryId = req.params.categoryId;
+  pool.query(
+    "SELECT * FROM subcategory WHERE category_id = $1",
+    [categoryId],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).send(result.rows)
-    })
-}
-
+      res.status(200).send(result.rows);
+    }
+  );
+};
 
 //get a products catagory from database
 
-const getProducts=(req,res) => {
-    const productCatagory = req.params.subcategoryId
-    pool.query('SELECT * FROM products WHERE subcategory_id = $1', [productCatagory],(error,result)=>{
-        if (error){
-            throw error;
-        }
+const getProducts = (req, res) => {
+  const productCatagory = req.params.subcategoryId;
+  pool.query(
+    "SELECT * FROM products WHERE subcategory_id = $1",
+    [productCatagory],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).send(result.rows)
-    })
-}
-
+      res.status(200).send(result.rows);
+    }
+  );
+};
 
 //get recent products catagory from database
 
-const getRecentProducts= (req,res) => {
-    pool.query('SELECT * FROM subcategory WHERE category_id = $1 ORDER BY id DESC LIMIT 10', [1]).then(
-        (agriculture) => {
-            pool.query('SELECT * FROM subcategory WHERE category_id = $1 ORDER BY id DESC LIMIT 10', [2]).then((horticulture) => {
-                pool.query('SELECT * FROM subcategory WHERE category_id = $1 ORDER BY id DESC LIMIT 10', [3], (error, handicrafts) => {
-                    if (error) {
-                        throw error;
-                    }
-                    res.status(200).send({Agriculture: agriculture.rows, Horticulture: horticulture.rows, Handicrafts: handicrafts.rows})
-                })
-            })
-        }
+const getRecentProducts = (req, res) => {
+  pool
+    .query(
+      "SELECT * FROM subcategory WHERE category_id = $1 ORDER BY id DESC LIMIT 10",
+      [1]
     )
-}
-
+    .then((agriculture) => {
+      pool
+        .query(
+          "SELECT * FROM subcategory WHERE category_id = $1 ORDER BY id DESC LIMIT 10",
+          [2]
+        )
+        .then((horticulture) => {
+          pool.query(
+            "SELECT * FROM subcategory WHERE category_id = $1 ORDER BY id DESC LIMIT 10",
+            [3],
+            (error, handicrafts) => {
+              if (error) {
+                throw error;
+              }
+              res.status(200).send({
+                Agriculture: agriculture.rows,
+                Horticulture: horticulture.rows,
+                Handicrafts: handicrafts.rows,
+              });
+            }
+          );
+        });
+    });
+};
 
 
 //get a single user from the database
 
-const getUserbyId=(req,res)=>{
-    const id= req.params.id
+const getUserbyId = (req, res) => {
+  const id = req.params.id;
 
-    pool.query('SELECT * FROM mock_data WHERE id=$1',[id],(error,result)=> {
-        if (error){
-            throw error
-        }
-        res.status(200).send(result.rows)
-    })
-}
-
+  pool.query("SELECT * FROM mock_data WHERE id=$1", [id], (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(result.rows);
+  });
+};
 
 //add new products
 
-const createProduct=(req,res) => {
-    const body = req.body
-    const table = req.params.table_name
+const createProduct = (req, res) => {
+  const body = req.body;
+  const table = req.params.table_name;
 
-    pool.query(`INSERT INTO ${table} (name, description, price, image, quantity, user_id) VALUES ($1, $2, $3, $4, $5, $6)`,
-    [body.name, body.description, body.price, body.image, body.quantity, body.user_id, body.user_id], (error,result) => {
-        if (error){
-            throw error;
-        }
-        res.status(200).send('added successfully')
-    })
-}
-
+  pool.query(
+    `INSERT INTO ${table} (name, description, price, image, quantity, user_id) VALUES ($1, $2, $3, $4, $5, $6)`,
+    [
+      body.name,
+      body.description,
+      body.price,
+      body.image,
+      body.quantity,
+      body.user_id,
+      body.user_id,
+    ],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send("added successfully");
+    }
+  );
+};
 
 //add a product to favorite list by user id and product id
 
-const addProductTOFavorite=(req,res)=> {
-    const body = req.body
+const addProductTOFavorite = (req, res) => {
+  const body = req.body;
 
-    pool.query('INSERT INTO favorite (product_id, user_id) VALUES ($1, $2)', [body.productId, body.userId], (error,result) => {
-        if (error){
-            throw error
-        }
+  pool.query(
+    "INSERT INTO favorite (product_id, user_id) VALUES ($1, $2)",
+    [body.productId, body.userId],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).send('added successfully')
-    })
-}
-
+      res.status(200).send("added successfully");
+    }
+  );
+};
 
 //get all favorite products by user id
 
-const getFavoriteProducts=(req,res)=> {
-    const userId = req.params.userId
-    console.log('The user id is', userId);
-    pool.query('SELECT * FROM favorite INNER JOIN products ON favorite.product_id = products.id WHERE favorite.user_id = $1', [userId], (error,result) => {
-        if (error){
-            throw error
-        }
+const getFavoriteProducts = (req, res) => {
+  const userId = req.params.userId;
+  console.log("The user id is", userId);
+  pool.query(
+    "SELECT * FROM favorite INNER JOIN products ON favorite.product_id = products.id WHERE favorite.user_id = $1",
+    [userId],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).send(result.rows)
-    })
-}
-
+      res.status(200).send(result.rows);
+    }
+  );
+};
 
 //add a product to cart table by user id and product id
 
-const addProductToCart=(req, res)=> {
-    const body = req.body
+const addProductToCart = (req, res) => {
+  const body = req.body;
 
-    pool.query('INSERT INTO cart (user_id, product_id, quantity) VALUES ($1, $2, $3)', [body.userId, body.productId, body.quantity], (error,result) => {
-        if (error){
-            throw error
-        }
+  pool.query(
+    "INSERT INTO cart (user_id, product_id, quantity) VALUES ($1, $2, $3)",
+    [body.userId, body.productId, body.quantity],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).send('added successfully')
-    })
-}
-
+      res.status(200).send("added successfully");
+    }
+  );
+};
 
 //get all favorite products by user id
 
-const getProductsFromCart=(req,res)=> {
-    const userId = req.params.userId
-    console.log('The user id is', userId);
-    pool.query('SELECT * FROM cart INNER JOIN products ON cart.product_id = products.id WHERE cart.user_id = $1', [userId], (error,result) => {
-        if (error){
-            throw error
-        }
+const getProductsFromCart = (req, res) => {
+  const userId = req.params.userId;
+  console.log("The user id is", userId);
+  pool.query(
+    "SELECT * FROM cart INNER JOIN products ON cart.product_id = products.id WHERE cart.user_id = $1",
+    [userId],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).send(result.rows)
-    })
-}
-
+      res.status(200).send(result.rows);
+    }
+  );
+};
 
 //delete an user from the db
 
-const deleteUser=(req,res)=>{
-    const id=req.param.id
+const deleteUser = (req, res) => {
+  const id = req.param.id;
 
-    pool.query('DELETE FROM users WHERE id=$1',[id],(error,result)=>{
-        if(error){
-            throw error
-        }
+  pool.query("DELETE FROM users WHERE id=$1", [id], (error, result) => {
+    if (error) {
+      throw error;
+    }
 
-        res.status(200).send(`user deleted with id ${id}`)
-    })
-}
-
+    res.status(200).send(`user deleted with id ${id}`);
+  });
+};
 
 module.exports = {
-    getUsersById,
-    getUserbyId,
-    updateUser,
-    deleteUser,
-    getProducts,
-    createUser,
-    loginUser,
-    getRecentProducts,
-    createProduct,
-    getCategory,
-    getSubCategory,
-    addProductTOFavorite,
-    getFavoriteProducts,
-    addProductToCart,
-    getProductsFromCart,
-}
+  getUsersById,
+  getUserbyId,
+  updateUser,
+  deleteUser,
+  getProducts,
+  createUser,
+  loginUser,
+  getRecentProducts,
+  createProduct,
+  getCategory,
+  getSubCategory,
+  addProductTOFavorite,
+  getFavoriteProducts,
+  addProductToCart,
+  getProductsFromCart,
+};
